09398fcc1146dc365d201f081698768c33986500
Jun Hyuk Han <hanjun.1998@gmail.com>
2020-07-16 14:50:56 +0900

    Created a function to calculate the end time of the sub.

--- ui/src/FirstimeLang.js
+++ ui/src/FirstimeLang.js
@@ -7,10 +7,11 @@ import './FirstimeLang.css';
 class TranscriptRow extends React.Component {
     render() {
         const line = this.props.line;
+        const end = calculateEndTime(line.tStartMs, line.dDurationMs)
         return (
             <tr>
-                <td>{line.start}-{line.end}</td>
-                <td>{line.text}</td>
+                <td>{msToTime(line.tStartMs)} - {msToTime(end)}</td>
+                <td>{line.segs[0].utf8}</td>
             </tr>
         );
     }
@@ -20,10 +21,11 @@ class TranscriptTable extends React.Component {
     render() {
         const rows = [];
         this.props.transcript.forEach((line) => {
+
             rows.push(
                 <TranscriptRow 
                     line ={line}
-                    key={line.start}
+                    key={line.tStartMs}
                 />
             )
         });
@@ -42,16 +44,31 @@ class TranscriptTable extends React.Component {
     }
 }
 
-
+class Search extends React.Component {
+    render() {
+        return (
+            <form onSubmit={this.props.onSubmit}>
+                <label>
+                    Link:
+                    <input type="text" onChange={this.props.onChange} />
+                </label>
+                    <input type="submit" value="Submit" />
+            </form>
+        );
+    }
+}
 
 class Lang extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            transcript: sub.Transcript
+            link: "http://video.google.com/timedtext?v=dL5oGKNlR6I&lang=de&fmt=json3",
+            transcript: sub.events
         };
-
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
+    
     
     // componentDidMount() {
     //     axios.get(`http://localhost:8000/subtitle`)
@@ -61,10 +78,30 @@ class Lang extends React.Component {
     //         this.setState({ transcript });
     //     });
     // }
+    handleChange(event) {
+        this.setState({link: event.target.value});
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        e.preventDefault();
+        console.log("button clicked");
+        axios.get(this.state.link)
+        .then(res => {
+            console.log(res.data.events)
+            const transcript = res.data.events;
+            this.setState({ transcript });
+        });
+    }
     
-      render() {
+    render() {
         return (
-                <div class="lang">
+                <div className="lang">
+                    <Search 
+                        link = {this.state.link}
+                        onSubmit={(e) => this.handleSubmit(e)}
+                        onChange={(e) => this.handleChange(e)}
+                    />,
                     <TranscriptTable 
                         transcript = {this.state.transcript}
                     />
@@ -74,6 +111,111 @@ class Lang extends React.Component {
     // axios.get('video.google.com/timedtext?lang=de&v=dL5oGKNlR6I');
 }
 
-const sub = {"Transcript":[{"text":"So etwas gab es noch nie.","start":"1.2","Dur":"2.04","end":"3.24"},{"text":"Nicht mal im Guinness-Buch der Rekorde.","start":"5.12","Dur":"1.88","end":"7.00"},{"text":"Falls doch, schreibts in die Kommentare.","start":"7.28","Dur":"1.92","end":"9.20"},{"text":"- Ahhh Leute!","start":"9.96","Dur":"1","end":"10.96"},{"text":"Heute kommt Meinberg an seine Grenzen.","start":"12.12","Dur":"2.44","end":"14.56"},{"text":"Ich bin bereit, für alles.","start":"17.8","Dur":"1.32","end":"19.12"},{"text":"Mein neues Highlight.","start":"20.12","Dur":"1.12","end":"21.24"},{"text":"- Nee.","start":"26.32","Dur":"1","end":"27.32"},{"text":"- (lachend) Doch!","start":"27.64","Dur":"1.16","end":"28.80"},{"text":"Das geht nicht. Da löst sich meine Haut ab.","start":"32.76","Dur":"2.92","end":"35.68"},{"text":"Die kann ich hinterher abziehen.","start":"36.32","Dur":"1.52","end":"37.84"},{"text":"Ach nee, ey.","start":"38.16","Dur":"1","end":"39.16"},{"text":"Meinberg, ich hab versucht, einen Arzt erreichen, ging nicht ran.","start":"40.76","Dur":"3.16","end":"43.92"},{"text":"Es wird schon klappen.","start":"45.52","Dur":"1.04","end":"46.56"},{"text":"* Musik: George Michael - Careless Whisper *","start":"48.24","Dur":"3.76","end":"52.00"},{"text":"Auf die Plätze, fertig, los.","start":"55.24","Dur":"1.8","end":"57.04"},{"text":"- Das Problem ist, jetzt erst mal fühlt sich das Mega angenehm an.","start":"61.96","Dur":"4","end":"65.96"},{"text":"- Aber du hast ja nur noch 23 Stunden, 59 Minuten und 45 Sekunden.","start":"67.92","Dur":"4.96","end":"72.88"},{"text":"Dort ist ne Kamera, die läuft durch. 24 Stunden.","start":"76.48","Dur":"3.2","end":"79.68"},{"text":"Auch wenn ich im Nachhinein sehe, dass sie den Pool verlassen hast,","start":"80.68","Dur":"2.96","end":"83.64"},{"text":"verlierst du.","start":"83.72","Dur":"0.88","end":"84.60"},{"text":"- Ich darf gar nicht raus?","start":"85.4","Dur":"2.6","end":"88.00"},{"text":"- Auf den Moment habe ich mich so gefreut,","start":"88.6","Dur":"1.84","end":"90.44"}]};
+function msToTime(ms) {   
+    var str = ''
+    if (ms > 3600000) {
+        var hr = Math.floor(ms / 3600000);
+        str += hr + ':';
+        ms -= (hr * 3600000);
+    }
+    if (ms > 60000) {
+        var min = Math.floor(ms / 60000);
+        str += min + ':';
+        ms -= (min * 60000);
+    }
+    str += ms/1000;
+    
+    return str
+}
+
+function calculateEndTime(start, dur) {
+    return start + dur
+}
+
+const sub = {"events": [ {
+    "tStartMs": 1200,
+    "dDurationMs": 2040,
+    "segs": [ {
+      "utf8": "So etwas gab es noch nie."
+    } ]
+  }, {
+    "tStartMs": 5120,
+    "dDurationMs": 1880,
+    "segs": [ {
+      "utf8": "Nicht mal im Guinness-Buch\nder Rekorde."
+    } ]
+  }, {
+    "tStartMs": 7280,
+    "dDurationMs": 1920,
+    "segs": [ {
+      "utf8": "Falls doch,\nschreibts in die Kommentare."
+    } ]
+  }, {
+    "tStartMs": 9960,
+    "dDurationMs": 1000,
+    "segs": [ {
+      "utf8": "- Ahhh Leute!"
+    } ]
+  }, {
+    "tStartMs": 12120,
+    "dDurationMs": 2440,
+    "segs": [ {
+      "utf8": "Heute kommt Meinberg\nan seine Grenzen."
+    } ]
+  }, {
+    "tStartMs": 17800,
+    "dDurationMs": 1320,
+    "segs": [ {
+      "utf8": "Ich bin bereit, für alles."
+    } ]
+  }, {
+    "tStartMs": 20120,
+    "dDurationMs": 1120,
+    "segs": [ {
+      "utf8": "Mein neues Highlight."
+    } ]
+  }, {
+    "tStartMs": 26320,
+    "dDurationMs": 1000,
+    "segs": [ {
+      "utf8": "- Nee."
+    } ]
+  }, {
+    "tStartMs": 27640,
+    "dDurationMs": 1160,
+    "segs": [ {
+      "utf8": "- (lachend) Doch!"
+    } ]
+  }, {
+    "tStartMs": 32760,
+    "dDurationMs": 2920,
+    "segs": [ {
+      "utf8": "Das geht nicht.\nDa löst sich meine Haut ab."
+    } ]
+  }, {
+    "tStartMs": 36320,
+    "dDurationMs": 1520,
+    "segs": [ {
+      "utf8": "Die kann ich hinterher abziehen."
+    } ]
+  }, {
+    "tStartMs": 38160,
+    "dDurationMs": 1000,
+    "segs": [ {
+      "utf8": "Ach nee, ey."
+    } ]
+  }, {
+    "tStartMs": 40760,
+    "dDurationMs": 3160,
+    "segs": [ {
+      "utf8": "Meinberg, ich hab versucht, einen\nArzt erreichen, ging nicht ran."
+    } ]
+  }, {
+    "tStartMs": 454520,
+    "dDurationMs": 1040,
+    "segs": [ {
+      "utf8": "Es wird schon klappen."
+    } ]
+}]};
 
 export default Lang
